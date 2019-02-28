4417c0f004af9f225a0c915ac07ae4aa609324ce
holydogs <525443029@qq.com>
2019-02-28 17:00:40 +0800

    包名重命名

--- Survey-Creater/src/main/java/com/me/config/RedisConfig.java
+++ Survey-Creater/src/main/java/com/me/config/RedisConfig.java
@@ -1,24 +1,18 @@
-package com.me.Config;
+package com.me.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurationSelector;
-import org.springframework.cache.annotation.CachingConfigurer;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
- * @ClassName Config
+ *
+ * @ClassName config
  * @Description TODO
  * @Author xufeng
  * @Data 2019/2/28 15:30
