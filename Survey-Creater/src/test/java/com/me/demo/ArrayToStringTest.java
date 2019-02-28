f0147eb0fc0d81878116547caa5661d13303c4a2
holydogs <525443029@qq.com>
2019-02-28 18:20:39 +0800

    修复boolean类型插入数据库时的类型错误bug

---
+++ Survey-Creater/src/test/java/com/me/demo/ArrayToStringTest.java
@@ -0,0 +1,11 @@
+package com.me.demo;
+
+/**
+ * @ClassName ArrayToStringTest
+ * @Description TODO
+ * @Author xufeng
+ * @Data 2019/2/28 17:43
+ * @Version 1.0
+ **/
+public class ArrayToStringTest {
+}
